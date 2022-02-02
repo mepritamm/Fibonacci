b160442bc219f2cff1b62736f5ccad625dc559f8
mepritamm <41336862+mepritamm@users.noreply.github.com>
2022-02-02 16:13:23 +0530

    Fibonacci.java added

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+
+    public void printFibonacci(int n) {
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        int count = 2;
+        System.out.println(a);
+        System.out.println(b);
+        while (count < n) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(b);
+            count++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Fibonacci obj = new Fibonacci();
+        obj.printFibonacci(10);
+    }
+}
